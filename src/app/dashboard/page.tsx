0bbee663d64c5cc4deee7db640a9f2cecf035b39
Paxxs <10364775+Paxxs@users.noreply.github.com>
2024-03-01 02:43:31 +0800

    fix: Fix import statement in dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,13 +1,12 @@
-"use client"
+"use client";
 
-import EvoEditor from "@/components/evo-note/editor"
-
+import EvoEditor from "@/components/evo-note/evo-editor";
 
 export default function Page(): JSX.Element {
-    return (
-        <div className="flex w-full">
-            <EvoEditor />
-            {/* <h1>Dashboard</h1> */}
-        </div>
-    );
+  return (
+    <div className="flex w-full">
+      <EvoEditor />
+      {/* <h1>Dashboard</h1> */}
+    </div>
+  );
 }

35cedf4713859f4ab383f508302f3ebe6f488d65
Paxxs <10364775+Paxxs@users.noreply.github.com>
2024-03-03 00:00:18 +0800

    chore: try turbo :)

--- apps/evo-editor/src/components/evo-note/core/tw-editor.tsx
+++ apps/evo-editor/src/components/evo-note/core/tw-editor.tsx
@@ -3,7 +3,9 @@ import { EditorRoot } from "novel";
 export default function TwEditor() {
   return (
     <>
-      <EditorRoot></EditorRoot>
+      <EditorRoot>
+        <div></div>
+      </EditorRoot>
     </>
   );
 }

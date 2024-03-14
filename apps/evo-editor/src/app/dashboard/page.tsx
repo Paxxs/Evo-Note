3bf975ef9d1f6697baaf7e29a9a386d1c322351e
Paxxs <10364775+Paxxs@users.noreply.github.com>
2024-03-14 20:55:14 +0800

    feat: Add loading skeleton to EvoEditor dynamic import
    This commit introduces a loading skeleton for the EvoEditor component
    using dynamic import to enhance user experience during component load.
    It also includes the addition of the EvoSkeleton component and its
    styling within the evo-note section, providing a visual placeholder
    before the editor is fully loaded.

--- apps/evo-editor/src/app/dashboard/page.tsx
+++ apps/evo-editor/src/app/dashboard/page.tsx
@@ -1,11 +1,13 @@
-"use client";
+// "use client";
 
 // import EvoEditor from "@/components/evo-note/evo-editor";
+import EvoSkeleton from "@/components/evo-note/skeleton";
 import dynamic from "next/dynamic";
 
 export default function Page(): JSX.Element {
   const EvoEditor = dynamic(() => import("@/components/evo-note/evo-editor"), {
     ssr: false,
+    loading: () => <EvoSkeleton />,
   });
   return (
     <div className="flex w-full" vaul-drawer-wrapper="">
---
+++ apps/evo-editor/src/components/evo-note/skeleton.tsx
@@ -0,0 +1,55 @@
+import { Skeleton } from "../ui/skeleton";
+
+export default function EvoSkeleton() {
+  return (
+    <>
+      <div className="flex flex-col w-full">
+        <div className="flex flex-row items-center justify-between border-b select-none h-9 space-x-7">
+          <Skeleton className="ml-2 h-5 w-60" />
+          <Skeleton className="h-5 w-30" />
+        </div>
+        <div className="flex h-full items-stretch">
+          <div className="flex flex-col w-1/5">
+            <div className="h-[52px] px-2 border-b items-center justify-center">
+              <Skeleton className="h-10  mt-1" />
+            </div>
+            <div className="flex flex-grow">
+              {/* 侧栏 */}
+              <div className="flex flex-col gap-4 py-2 px-2 flex-grow">
+                <Skeleton className="h-10  mt-1" />
+                <Skeleton className="h-10  mt-1" />
+                <Skeleton className="h-10  mt-1" />
+                <Skeleton className="h-10  mt-1" />
+                <Skeleton className="h-10 mt-auto" />
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col w-[30%]">
+            <div className="h-[52px] p-2 border-b border-x">
+              <Skeleton className="h-7 w-14 mt-1 ml-3" />
+            </div>
+            <div className="flex flex-col flex-grow border-x gap-2 p-3">
+              <Skeleton className="h-28" />
+              <Skeleton className="h-28" />
+              <Skeleton className="h-28" />
+              {/* 侧栏 */}
+            </div>
+          </div>
+          <div className="flex flex-col flex-grow">
+            <div className="h-[52px] px-2 border-b">
+              <Skeleton className="h-7 w-20 mt-3 ml-3" />
+            </div>
+            <div className="flex flex-col flex-grow gap-3 p-3">
+              <Skeleton className="h-28" />
+              <div className="space-y-2">
+                <Skeleton className="h-4 w-[250px]" />
+                <Skeleton className="h-4 w-[200px]" />
+              </div>
+              {/* 侧栏 */}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}

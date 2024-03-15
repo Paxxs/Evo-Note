2a7e85abf2f233878cba23441665ff480f6f4a5c
Paxxs <10364775+Paxxs@users.noreply.github.com>
2024-03-16 00:15:55 +0800

    feat: Prevent extra scrollbar issue within HTML

--- apps/evo-editor/src/app/layout.tsx
+++ apps/evo-editor/src/app/layout.tsx
@@ -4,7 +4,6 @@ import { Noto_Sans_SC } from "next/font/google";
 import "./globals.css";
 
 import { cn } from "@/lib/utils";
-import { ThemeProvider } from "@/components/theme-provider";
 import Provider from "./providers";
 
 const fontSans = FontSans({
@@ -28,7 +27,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning className="overflow-hidden">
       {/* <body className={inter.className}>{children}</body> */}
       <body
         className={cn(

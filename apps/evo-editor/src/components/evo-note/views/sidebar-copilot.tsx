02fb747e6fa68bface66d221ce935f71ac4174f0
Paxxs <10364775+Paxxs@users.noreply.github.com>
2024-03-17 00:07:57 +0800

    feat: Implement Copilot Chat Page using a simple embedded iframe, with plans to complete this feature later.

--- apps/evo-editor/src/components/evo-note/views/sidebar-copilot.tsx
+++ apps/evo-editor/src/components/evo-note/views/sidebar-copilot.tsx
@@ -4,6 +4,13 @@ export default function SideBarAI() {
   return (
     <>
       <SideBarTitle title="Copilot Chat"></SideBarTitle>
+      <iframe
+        src="https://freegpt.nb.gl"
+        className="w-full h-[calc(100dvh-52.8px-3rem)]"
+        allow="autoplay; encrypted-media; fullscreen"
+        sandbox="allow-scripts allow-same-origin allow-forms"
+        loading="lazy"
+      ></iframe>
     </>
   );
 }
--- apps/evo-editor/src/components/evo-note/views/sidebar-trash.tsx
+++ apps/evo-editor/src/components/evo-note/views/sidebar-trash.tsx
@@ -5,7 +5,13 @@ export default function SidebarTrash() {
   return (
     <>
       <SideBarTitle title="Trash"></SideBarTitle>
-      <FileList
+      <div className="flex flex-col items-center justify-center px-3 align-baseline pt-20">
+        <div>🥰</div>
+        <div className="text-lg font-semibold whitespace-nowrap">
+          This feature is not ready
+        </div>
+      </div>
+      {/* <FileList
         className="pt-3"
         files={[
           {
@@ -16,18 +22,10 @@ export default function SidebarTrash() {
             createdTime: "2020-05-07 01:24:49",
             lastModified: "2020-12-18 18:48:08",
             stars: false,
-          },
-          {
-            id: "notes/note_2.md",
-            name: "Lunar Serenade",
-            brief:
-              "A walk in nature is a perfect moment to reflect on life's vastness and beauty. 今天我学习了如何使用Python进行数据分析，非常有趣。 我尝试用水彩画风景，虽然不专业，但很享受过程。 Learning a new language opens up a window to a different culture and world view. The history of ancient civilizations is both fascinating and enlightening.",
-            createdTime: "2022-09-25 22:53:55",
-            lastModified: "2021-07-13 11:37:29",
-            stars: false,
+            tags: [],
           },
         ]}
-      />
+      /> */}
     </>
   );
 }

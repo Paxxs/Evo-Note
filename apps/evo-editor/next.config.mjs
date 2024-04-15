e3c66d462b5309904fda2aca2b78d346d8ae4182
Paxxs <10364775+Paxxs@users.noreply.github.com>
2024-04-15 23:12:34 +0800

    feat: Simplified implementation of metrics collection for development version information

--- apps/evo-editor/next.config.mjs
+++ apps/evo-editor/next.config.mjs
@@ -1,9 +1,34 @@
+import { execSync } from "child_process";
+
+const gitInfo = execSync('git log -1 --pretty=format:"%h|%an|%ae"')
+  .toString()
+  .trim()
+  .split("|"); // 将返回结果分割为数组，其中包含哈希、作者名和作者邮箱
+
+const commitHash = gitInfo[0];
+const lastAuthorName = gitInfo[1];
+const lastAuthorEmail = gitInfo[2];
+
+const effectiveEmail = lastAuthorEmail.includes("noreply.github.com")
+  ? "pm"
+  : lastAuthorEmail;
+const lastAuthor = `${lastAuthorName}<${effectiveEmail}>`;
+
+const repositoryUrl = execSync("git config --get remote.origin.url")
+  .toString()
+  .trim();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   output: "export",
   trailingSlash: true,
   distDir: "../go-backend/dist",
+  env: {
+    GIT_HASH: commitHash,
+    LAST_GIT_AUTHOR: lastAuthor,
+    REPOSITORY_URL: repositoryUrl,
+  },
 };
 
 export default nextConfig;
---
+++ apps/evo-editor/src/app/merics/index.tsx
@@ -0,0 +1,11 @@
+import Clarity from "./msc";
+import Plausible from "./pa";
+
+const Metrics = () => (
+  <>
+    <Clarity />
+    <Plausible />
+  </>
+);
+
+export default Metrics;
